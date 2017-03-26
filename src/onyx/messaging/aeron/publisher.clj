3a87fa44ac8d5c45cee33559a603a247ca5be0ba
Lucas Bradstreet <lucasbradstreet@gmail.com>
2017-03-26 13:45:12 -0700

    New encoder.

--- src/onyx/messaging/aeron/publisher.clj
+++ src/onyx/messaging/aeron/publisher.clj
@@ -3,21 +3,75 @@
             [onyx.messaging.protocols.endpoint-status :as endpoint-status]
             [onyx.messaging.protocols.publisher :as pub]
             [onyx.messaging.aeron.endpoint-status :refer [new-endpoint-status]]
-            [onyx.messaging.aeron.utils :as autil :refer [action->kw stream-id heartbeat-stream-id max-message-length]]
+            [onyx.messaging.aeron.utils :as autil
+             :refer [action->kw stream-id heartbeat-stream-id max-message-length]]
             [onyx.messaging.serialize :as sz]
             [onyx.peer.constants :refer [NOT_READY ENDPOINT_BEHIND]]
             [onyx.types :refer [heartbeat ready]]
+            [onyx.messaging.serialize :as sz]
             [onyx.compression.nippy :refer [messaging-compress messaging-decompress]]
             [taoensso.timbre :refer [debug info warn] :as timbre])
   (:import [io.aeron Aeron Aeron$Context Publication UnavailableImageHandler AvailableImageHandler]
            [java.util.concurrent.atomic AtomicLong]
+           [onyx.serialization MessageEncoder MessageDecoder MessageEncoder$SegmentsEncoder]
            [org.agrona.concurrent UnsafeBuffer]
            [org.agrona ErrorHandler]))
 
-(deftype Publisher [peer-config src-peer-id dst-task-id slot-id site 
-                    ^AtomicLong written-bytes ^AtomicLong errors 
-                    ^Aeron conn ^Publication publication status-mon error
-                    ^:unsynchronized-mutable short-id ^:unsynchronized-mutable replica-version 
+(def enc (-> (sz/->SegmentsEncoder (UnsafeBuffer. (byte-array 100000)) nil nil)
+             (sz/wrap 0)
+             (sz/add-message (messaging-compress {:n 1}))
+             (sz/add-message (messaging-compress {:n 2}))
+             (sz/add-message (messaging-compress {:n 3}))
+             (sz/add-message (messaging-compress {:n 4}))
+             ;(sz/add-message (messaging-compress {:n 5}))
+             ;(sz/add-message (messaging-compress {:n 6}))
+             ))
+
+(defprotocol PSegmentDecoder
+  (nextv [this])
+  (length [this])
+  (wrap [this ^UnsafeBuffer buffer offset]))
+
+(deftype SegmentDecoder [^bytes bs
+                         ^:unsynchronized-mutable ^UnsafeBuffer buffer
+                         ^:unsynchronized-mutable n
+                         ^:unsynchronized-mutable offset]
+  PSegmentDecoder
+  (wrap [this new-buffer new-offset]
+    (set! buffer new-buffer)
+    (set! n (.getShort ^UnsafeBuffer new-buffer new-offset))
+    (set! offset (unchecked-add-int new-offset 2))
+    this)
+  (length [this] n)
+  (nextv [this]
+    (when (pos? n)
+      (let [msg-length (.getInt buffer offset)
+            new-offset (unchecked-add-int offset 4)
+            _ (.getBytes buffer new-offset bs 0 msg-length)] 
+        (set! n (unchecked-add-int n -1))
+        (set! offset (unchecked-add-int new-offset msg-length))
+        bs))))
+
+(do (def decoder (->SegmentDecoder (byte-array 1000000) nil nil nil))
+    (wrap decoder (.buffer enc) 0)
+    (let [vs (transient [])] 
+      (loop []
+        (when-let [bs (nextv decoder)]
+          (conj! vs (messaging-decompress bs))
+          (recur)))
+      (persistent! vs)))
+
+(identity nn)
+
+
+
+;; maintain segments in here
+(deftype Publisher [peer-config src-peer-id dst-task-id slot-id site ^UnsafeBuffer buffer 
+                    ^:unsynchronized-mutable ^MessageEncoder encoder 
+                    ^:unsynchronized-mutable ^MessageEncoder segments-encoder 
+                    ^AtomicLong written-bytes 
+                    ^AtomicLong errors ^Aeron conn ^Publication publication status-mon error 
+                    ^:unsynchronized-mutable short-id ^:unsynchronized-mutable replica-version
                     ^:unsynchronized-mutable epoch]
   pub/Publisher
   (info [this]
@@ -78,8 +132,8 @@
                               {:media-driver/max-length (.maxPayloadLength pub)
                                :publication/max-length (max-message-length)})))
           status-mon (endpoint-status/start (new-endpoint-status peer-config src-peer-id (.sessionId pub)))]
-      (Publisher. peer-config src-peer-id dst-task-id slot-id site written-bytes errors conn
-                  pub status-mon error short-id replica-version epoch))) 
+      (Publisher. peer-config src-peer-id dst-task-id slot-id site buffer encoder written-bytes 
+                  errors conn pub status-mon error short-id replica-version epoch))) 
   (stop [this]
     (info "Stopping publisher" (pub/info this))
     (when status-mon (endpoint-status/stop status-mon))
@@ -88,8 +142,8 @@
      (catch io.aeron.exceptions.RegistrationException re
        (info "Registration exception stopping publisher:" re)))
     (when conn (.close conn))
-    (Publisher. peer-config src-peer-id dst-task-id slot-id site written-bytes 
-                errors nil nil nil error nil nil nil))
+    (Publisher. peer-config src-peer-id dst-task-id slot-id site buffer encoder
+                written-bytes errors nil nil nil error nil nil nil))
   (endpoint-status [this]
     status-mon)
   (ready? [this]
@@ -102,6 +156,23 @@
           ret (.offer ^Publication publication buf 0 (.capacity buf))]
       (debug "Offered ready message:" [ret msg :session-id (.sessionId publication) :site site])
       ret))
+  (reset-encoder! [this]
+    (set! encoder 
+          (-> encoder
+              ;; offset by 1 byte, as message type is encoded
+              (.wrap buffer 1)
+              (.replicaVersion replica-version))))
+  (add-segment! [this bs]
+    (let [;bs ^bytes (messaging-compress v) 
+          (.count )
+          cnt ^int (alength bs)]
+      (when v 
+        (recur (.putSegmentBytes (.next enc) bs 0 cnt)
+               (first vs) 
+               (rest vs))))
+
+    
+    )
   (offer-heartbeat! [this]
     (let [msg (heartbeat replica-version epoch src-peer-id :any (.sessionId publication) short-id)
           buf (sz/serialize msg)
@@ -129,10 +200,14 @@
           ENDPOINT_BEHIND)))
 
 (defn new-publisher 
-  [peer-config monitoring {:keys [src-peer-id dst-task-id slot-id site short-id] :as pub-info}]
-  (->Publisher peer-config src-peer-id dst-task-id slot-id site 
-               (:written-bytes monitoring) (:publication-errors monitoring) nil 
-               nil nil (atom nil) short-id nil nil))
+  [peer-config {:keys [written-bytes publication-errors] :as monitoring}
+   {:keys [src-peer-id dst-task-id slot-id site short-id] :as pub-info}]
+  (let [bs (byte-array (max-message-length)) 
+        buffer (UnsafeBuffer. bs)
+        encoder (MessageEncoder.)
+        _ (sz/put-message-type buffer 0 sz/message-id)]
+    (->Publisher peer-config src-peer-id dst-task-id slot-id site buffer encoder written-bytes
+                 publication-errors nil nil  nil (atom nil) short-id nil nil)))
 
 (defn reconcile-pub [peer-config monitoring publisher pub-info]
   (if-let [pub (cond (and publisher (nil? pub-info))
--- src/onyx/messaging/serialize.clj
+++ src/onyx/messaging/serialize.clj
@@ -3,6 +3,35 @@
   (:import [org.agrona.concurrent UnsafeBuffer]
            [onyx.serialization MessageEncoder MessageDecoder MessageEncoder$SegmentsEncoder]))
 
+(defprotocol PSegmentEncoder
+  (add-message [this bs])
+  (offset [this])
+  (length [this])
+  (wrap [this offset]))
+
+(deftype SegmentsEncoder [^UnsafeBuffer buffer 
+                          ^:unsynchronized-mutable start-offset
+                          ^:unsynchronized-mutable offset]
+  PSegmentEncoder
+  (add-message [this bs]
+    (let [len (int (alength ^bytes bs))
+          _ (.putInt buffer offset len)
+          new-offset (unchecked-add-int offset 4)
+          new-count (short (unchecked-add-int (.getShort buffer start-offset) 1))]
+      (.putBytes buffer new-offset bs)
+      (.putShort buffer start-offset new-count)
+      (set! offset (unchecked-add-int new-offset len))
+      this))
+  (offset [this] offset)
+  (length [this]
+    (.getShort buffer start-offset))
+  (wrap [this new-offset] 
+    (set! start-offset new-offset)
+    (.putShort buffer new-offset (int 0))
+    (set! offset (+ new-offset 2))
+    this))
+
+
 (def message-id ^:const (byte 0))
 (def barrier-id ^:const (byte 1))
 (def heartbeat-id ^:const (byte 2))
